4946f75a5def26738073e57c490d737bbb58ff87
limarenko_aa <vega74@ua.fm>
2017-03-01 10:32:15 +0200

    01.03.2017

--- Teploset/App_Start/Startup.Auth.cs
+++ Teploset/App_Start/Startup.Auth.cs
@@ -1,11 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Security.Claims;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+using System.Web.Helpers;
 
-namespace Teploset.App_Start
+namespace Teploset
 {
-	public class Startup
-	{
-	}
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier; //используется для правильной работы аттрибута [ValidateAntiForgeryToken]
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Account/Login")
+            });
+        }
+    }
 }
---
+++ Teploset/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Teploset.Startup))]
+namespace Teploset
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
